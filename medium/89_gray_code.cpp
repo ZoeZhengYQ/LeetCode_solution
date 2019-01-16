8b23ca9b32bb1c15badf4496957640d1f4d63474
Yingqiao Zheng <yingqiao.zoe@gmail.com>
2019-01-15 21:52:05 -0500

    bit operation

---
+++ medium/89_gray_code.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    // solution 1:
+    // feature of gray code: mirror arrangement
+    /*
+    vector<int> grayCode(int n) {
+        vector<int> res (1, 0);
+        for (int i = 0; i < n; ++i) {
+            int size = res.size();
+            for (int j = size - 1; j >= 0; --j) {
+                res.push_back(res[j] | (1 << i));
+            }
+        }
+        return res;
+    }
+    */
+    
+    // solution 2:
+    // direct arrangement
+};
