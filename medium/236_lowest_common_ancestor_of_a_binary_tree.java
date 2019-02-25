23f46cd1f5a7fcdf39227c80a4cab0b6266acf57
Yingqiao Zheng <yingqiao.zoe@gmail.com>
2019-02-25 17:03:32 -0500

    LCA, BT

---
+++ medium/236_lowest_common_ancestor_of_a_binary_tree.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 边界条件
+        if (root == null || root == p || root == q) return root;
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        // 在左边和右边各找到了各自的点，然后就返回root
+        if (left != null && right != null) return root;
+        return (left != null)? left : right;
+    }
+}
