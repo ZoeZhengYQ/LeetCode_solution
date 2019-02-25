3b931cc0b6571fd866f4e82b75336236a6bf2250
Yingqiao Zheng <yingqiao.zoe@gmail.com>
2019-02-25 16:44:49 -0500

    LCA, BST

---
+++ easy/235_lowest_common_ancestor.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // iterative solution
+    // LCA in BST must be the node with value between p and q
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode cur = root;
+        while (true) {
+            if (cur.val > p.val && cur.val > q.val) {
+                cur = cur.left;
+            } else if (cur.val < p.val && cur.val < q.val) {
+                cur = cur.right;
+            } else {
+                return cur;
+            }
+        }
+    }
+    
+    // recursion solution
+    /*
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+    */
+}
