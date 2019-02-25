c96a6fcb7d7de9156941da807aec0731b9e5899d
Yingqiao Zheng <yingqiao.zoe@gmail.com>
2019-02-25 16:44:41 -0500

    tricky math, array

---
+++ hard/41_first_missing_positive.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) return 1;
+        
+        // range是左边部分idx + 1
+        int range = partition(nums);
+        
+        // 初始化为第range + 1，即前面range个都出现了
+        // 如果没有更新这就是答案
+        int res = range + 1;
+        
+        System.out.println("Range: " + range);
+        for (int i = 0; i < range; ++i) {
+            int idx = Math.abs(nums[i]);
+            if (idx > 0 && idx <= range) {
+                nums[idx-1] = (nums[idx-1] == 0)? -1 : -Math.abs(nums[idx-1]);    
+            }
+        }
+        
+        // 搜索完[1, range]， 如果没有那答案就是range + 1
+        for (int i = 0; i < range; ++i) {
+            if (nums[i] > 0) {
+                res = i+1;
+                break;
+            }
+        }
+        return res;
+    }
+    
+    
+    
+    
+    
+    // put all the element that is less or equal to 0 to the right
+    // return index of the left part + 1
+    private int partition(int[] arr) {
+        int r = arr.length - 1;
+        int l = 0;
+        
+        while (r >= 0 && arr[r] <= 0) {
+            r--;    // now r is the pointer to the right irrelavent part
+        }
+        
+        // 注意这边是<=, 否则很可能l=r那个其实也是大于0但是没有算到range里面
+        while (l <= r) {
+            if (arr[l] <= 0) {
+                swap(arr, l, r);
+                while (r >= 0 && arr[r] <= 0) {
+                    r--;
+                }
+            }
+            l++;
+        }
+        return l;
+    }
+    
+    // swap新思路！！
+    private void swap(int[] arr, int x, int y) {
+        arr[x] ^= arr[y];
+        arr[y] ^= arr[x];
+        arr[x] ^= arr[y];
+    }
+}
