dcf33f278b2a6e23d1a3d5afcffa6bf60a4fed8b
Yingqiao Zheng <yingqiao.zoe@gmail.com>
2019-03-04 12:53:23 -0500

    string

---
+++ other/compress_string.java
@@ -0,0 +1,34 @@
+/* compress string: aacbbbc -> a2c1b3c1 */
+// using String
+public static void main(String[] args) {
+    String a = "aaabbccaaaddj";
+
+    for(int i=0;i<a.length();i++){
+        int c=i+1;
+        int duplicateCharCount=1;
+        while(c<a.length()&&a.charAt(c)==a.charAt(i)){
+            ++c;
+            ++duplicateCharCount;
+        }
+
+        a=a.substring(0,i+1)+duplicateCharCount+a.substring(i+duplicateCharCount);
+        i++;
+    }
+    System.out.println(a);
+}
+
+// using StringBuilder
+public static String compress(String str) {
+    StringBuilder result = new StringBuilder();
+    int i;
+    int count = 0;
+    for(i=0; i< str.length() - 1;i++,count++) {
+        if (str.charAt(i) != str.charAt(i + 1)) {
+            result.append(str.charAt(i)).append(count);
+            count = 0;
+        }
+    }
+
+    result.append(str.charAt(i)).append(count);
+    return result.toString();
+}
