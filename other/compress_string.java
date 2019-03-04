ec94b2ac587f5ae0ffdb6932aa189a67a9d1b11b
Yingqiao Zheng <yingqiao.zoe@gmail.com>
2019-03-04 13:05:32 -0500

    string

--- other/compress_string.java
+++ other/compress_string.java
@@ -3,15 +3,15 @@
 public static void main(String[] args) {
     String a = "aaabbccaaaddj";
 
-    for(int i=0;i<a.length();i++){
-        int c=i+1;
-        int duplicateCharCount=1;
-        while(c<a.length()&&a.charAt(c)==a.charAt(i)){
+    for(int i = 0;i < a.length(); i++){
+        int c = i + 1;
+        int duplicateCharCount = 1;
+        while(c < a.length() && a.charAt(c) == a.charAt(i)){
             ++c;
             ++duplicateCharCount;
         }
 
-        a=a.substring(0,i+1)+duplicateCharCount+a.substring(i+duplicateCharCount);
+        a = a.substring(0, i + 1) + duplicateCharCount + a.substring(i + duplicateCharCount);
         i++;
     }
     System.out.println(a);
