18b03d53130e6a89d71aadd2ae7da557573bed57
Yingqiao Zheng <yingqiao.zoe@gmail.com>
2019-03-04 13:52:40 -0500

    update comment

--- hard/41_first_missing_positive.java
+++ hard/41_first_missing_positive.java
@@ -2,17 +2,20 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         if (nums == null || nums.length == 0) return 1;
         
-        // range是左边部分idx + 1，就是length
+        // range是左边部分idx + 1，即为length
         int range = partition(nums);
         
         // 初始化为第range + 1，即前面range个都出现了
         // 如果没有更新这就是答案
         int res = range + 1;
         
-        System.out.println("Range: " + range);
+        // System.out.println("Range: " + range);
         for (int i = 0; i < range; ++i) {
             int idx = Math.abs(nums[i]);
             if (idx > 0 && idx <= range) {
+                // 每个数字的状态都记录在i-1那边，因为array是从[0, range)算的
+                // 一共range个，答案的范围是[1, range+1]
+                // 注意如果原本是0得用-1来标记
                 nums[idx-1] = (nums[idx-1] == 0)? -1 : -Math.abs(nums[idx-1]);    
             }
         }
@@ -20,6 +23,7 @@ class Solution {
         // 搜索完[1, range]， 如果没有那答案就是range + 1
         for (int i = 0; i < range; ++i) {
             if (nums[i] > 0) {
+                // 因为记录的时候有1的偏移，所以答案应该是i+1
                 res = i+1;
                 break;
             }
