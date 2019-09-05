b9bf6cc78ad55f51880434a961204ba0839bf49c
ZoeZhengYQ <yingqiao.zoe@gmail.com>
2019-09-05 14:17:22 -0400

    BTree, Iterative, Recursion

--- medium/222_count_complete_tree_nodes.java
+++ medium/222_count_complete_tree_nodes.java
@@ -7,18 +7,40 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-// Recursion
-// Time: O((log N)^2)
 class Solution {
     public int countNodes(TreeNode root) {
-        int h = height(root);
-        
-        return h < 0 ? 0 : (height(root.right) == h-1 ? (1 << h) + countNodes(root.right) : (1 << (h-1)) + countNodes(root.left));
+        int height = height(root);
+        // Time: O(n) ? No! O(h^2)
+        return height == -1 ? 0 : 1 + countNodes(root.left) + countNodes(root.right);
+        // return height < 0 ? 0 : 
+        //     height(root.right) == height-1 ? 
+        //         (1 << height) + countNodes(root.right) : 
+        //         (1 << (height-1)) + countNodes(root.left);
     }
-    
-    // Count the height of the tree by going left
+    // Iterative Solution
+    // Time: O(height^2) -> O((log n)^2)
+    // Space: O(1)
+    /*
+    public int countNodes(TreeNode root) {
+        int res = 0;
+        int heightL = height(root);
+        while (root != null) {
+            int heightR = height(root.right);
+            if (heightR == heightL - 1) {
+                res += 1 << heightL; // 2 ^ (heightL-1) - 1 + 1(the root node)
+                root = root.right;
+            } else {
+                res += 1 << (heightL-1);
+                root = root.left;
+            } 
+            heightL--;
+        }
+        return res;
+    }
+    */
+
+    // one-node tree height is 0
     private int height(TreeNode node) {
-        return (node == null) ? -1 : (1 + height(node.left));
+        return node == null ? -1 : 1 + height(node.left);
     }
 }
