8c956b4c2984b229eeb1672a11294ea4774cccfb
ZoeZhengYQ <yingqiao.zoe@gmail.com>
2019-10-07 00:12:29 -0400

    Tree, recursive

---
+++ easy/572_subtree_of_another_tree.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // Tree Traversal!
+    // Time: O(n)
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null || t == null) return s == t;
+        if (s.val != t.val) return false;
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}
