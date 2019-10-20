fbdb880be2e6cf3eedcd352ed09a90c810ad0af7
ZoeZhengYQ <yingqiao.zoe@gmail.com>
2019-10-19 23:37:48 -0400

    preorder traversal

--- hard/297_serialize_and_deserialize_binary_tree.java
+++ hard/297_serialize_and_deserialize_binary_tree.java
@@ -7,6 +7,65 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// Use Pre order traversal: recursive
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        seHelper(sb, root);
+        String res = sb.toString();
+        res = res.substring(0, res.lastIndexOf(","));
+        return res;
+    }
+    
+    private void seHelper(StringBuilder sb, TreeNode node) {
+        if (node == null) {
+            sb.append("X,");
+            return;
+        } 
+        sb.append(node.val + ",");
+        seHelper(sb, node.left);
+        seHelper(sb, node.right);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] arr = data.split(",");
+        Queue<String> list = new LinkedList<>();
+        for (String a : arr) {
+            list.add(a);
+        }
+        return deHelper(list);
+    }
+    
+    private TreeNode deHelper(Queue<String> list) {
+        String cur = list.remove();
+        if (cur.equals("X")) return null;
+        TreeNode node = new TreeNode(Integer.valueOf(cur));
+        node.left = deHelper(list);
+        node.right = deHelper(list);
+        return node;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
+
+
+/*
 public class Codec {
     private String splitter = ",";
     private String nullNode = "X";
@@ -54,7 +113,7 @@ public class Codec {
         }
     }
 }
-
+*/
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
 // codec.deserialize(codec.serialize(root));
