b4efbac1f5b8270de4b0ada93819861a099d26ee
ZoeZhengYQ <yingqiao.zoe@gmail.com>
2019-10-21 01:03:55 -0400

    second time 3sum

--- medium/15_3sum.java
+++ medium/15_3sum.java
@@ -26,3 +26,45 @@ class Solution {
         return res;
     }
 }
+
+/*
+class Solution {
+    // n two sum
+    // Time: O(n^2)
+    private List<List<Integer>> res;
+    
+    public List<List<Integer>> threeSum(int[] nums) {
+        res = new ArrayList<>(); 
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; ++i) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                doTwoSum(nums, i);   
+            }
+        }
+        return res;
+    }
+    
+    private void doTwoSum(int[] nums, int start) {
+        if (start >= nums.length-2) return;
+        int i = start + 1, j = nums.length-1;
+        int num = nums[start];
+        while (i < j) {
+            int sum = num + nums[i] + nums[j];
+            if (sum < 0) {
+                i++;
+                while (i < j && nums[i] == nums[i-1]) { i++; }
+            } else if (sum > 0) {
+                j--;
+                while (j > i && nums[j] == nums[j+1]) { j--; }
+            } else {
+                res.add(Arrays.asList(num, nums[i], nums[j]));
+                // 注意这边也要更新i和j！
+                i++;
+                j--;
+                while (i < j && nums[i] == nums[i-1]) i++;
+                while (i < j && nums[i] == nums[j+1]) j--;
+            }  
+        }
+    }
+}
+*/
