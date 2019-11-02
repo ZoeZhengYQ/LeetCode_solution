5c0315c829736107af94b4f249a66690ae4adaf6
ZoeZhengYQ <yingqiao.zoe@gmail.com>
2019-11-02 13:47:06 -0400

    LRU cache

--- medium/146_LRU_cache.java
+++ medium/146_LRU_cache.java
@@ -3,7 +3,115 @@
 // double LL obeys LRU
 
 // 64ms, 64.29%
+class LRUCache {
+    
+    class DLLNode {
+        int key; 
+        int val;
+        DLLNode prev;
+        DLLNode next;
+        
+        DLLNode (int key, int val) {
+            this.key = key;
+            this.val = val;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+    
+    private final int capacity;
+    private int size;
+    private Map<Integer, DLLNode> map;
+    private DLLNode head;
+    private DLLNode tail;
 
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.map = new HashMap<>();
+        this.head = null;
+        this.tail = null;
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        DLLNode node = map.get(key);
+        moveToHead(node);
+        return node.val;
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            DLLNode node = map.get(key);
+            node.val = value;
+            moveToHead(node);
+            return;
+        }
+        while (size >= capacity) {
+            popTail();
+        }
+        DLLNode newNode = new DLLNode(key, value);
+        map.put(key, newNode);
+        addToHead(newNode);
+    }
+    
+    // A new cache block
+    private void addToHead(DLLNode node) {
+        if (size == 0) {
+            head = node;
+            tail = node;
+        } else {
+            node.next = head;
+            head.prev = node;
+            head = node;   
+        }
+        size++;
+        map.put(node.key, node);
+    }
+    
+    // A new reference to a existing block
+    private void moveToHead(DLLNode node) {
+        if (head == node) return;
+        if (tail == node) {
+            node.prev.next = null;
+            tail = node.prev;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+        node.prev = null;
+        node.next = head;
+        head.prev = node;
+        head = node;
+    }
+    
+    private DLLNode popTail() {
+        if (size == 0) return null;
+        DLLNode node = tail;
+        if (size == 1) {
+            tail = null;
+            head = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;    
+        }
+        map.remove(node.key);
+        size--;
+        return node;
+    }
+    
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
+/*
 class LRUCache {
     private int size;
     private final int capacity;
@@ -117,6 +225,7 @@ class LRUCache {
         size--;
         return;
     }
+    */
     
     /*
     private void printCache() {
