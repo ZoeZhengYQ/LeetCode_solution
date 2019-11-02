aedf4782cbafdadb336f7b85e2c914bc4f0ed04d
ZoeZhengYQ <yingqiao.zoe@gmail.com>
2019-11-02 16:47:48 -0400

    DP

--- hard/44_wildcard_matching.java
+++ hard/44_wildcard_matching.java
@@ -1,4 +1,32 @@
 class Solution {
+    // DP: O(mn)
+    public boolean isMatch(String s, String p) {
+        if (p == null || p.length() == 0) {
+            return (s == null || s.length() == 0);
+        }
+        p = p.replaceAll("\\*+", "*");
+        
+        int m = p.length(), n = s.length();
+        boolean[][] dp = new boolean[m+1][n+1];
+        
+        dp[0][0] = true;
+        dp[1][0] = (p.charAt(0) == '*');
+        
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(i-1) == '*') { // match any char
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                } else if (p.charAt(i-1) == '?' || p.charAt(i-1) == s.charAt(j-1)) { // match this char
+                    dp[i][j] = dp[i-1][j-1];
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+
+
+
 // 多开一维表示空的s和p
 // DP[i][j] 是指 s到i 和 p到j 互相match
 // 1. s[i] 和 p[j] match || p[i] == '?'
@@ -8,6 +36,7 @@ class Solution {
 // 3. 不match直接是false
 // Time: O(mn)
 // Space: O(mn)
+    /*
     public boolean isMatch(String s, String p) {
         if ((s == null || s.length() == 0) && (p == null || p.length() == 0)) return true;
         else if (p == null || p.length() == 0) return false;
@@ -34,4 +63,5 @@ class Solution {
         }
         return match[m][n];
     }
+    */
 }
