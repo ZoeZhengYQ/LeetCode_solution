5997b22be4d0e9cd0fd77c4f790ab327080bc258
ZoeZhengYQ <yingqiao.zoe@gmail.com>
2020-01-13 13:45:06 -0500

    Heap

--- hard/295_find_median_from_data_stream.java
+++ hard/295_find_median_from_data_stream.java
@@ -4,6 +4,7 @@ class MedianFinder {
     // Add: O(log n)
     private PriorityQueue<Integer> minHeap;
     private PriorityQueue<Integer> maxHeap;
+    // Neater to use a boolean value here
     private boolean isEven;
 
     /** initialize your data structure here. */
@@ -28,6 +29,45 @@ class MedianFinder {
         // Remember the 2.0 here!!
         return isEven ? (maxHeap.peek() + minHeap.peek()) / 2.0 : minHeap.peek();
     }
+
+    /*
+    private int size;
+    // The right half
+    private PriorityQueue<Integer> minHeap;
+    // The left half
+    private PriorityQueue<Integer> maxHeap;
+    
+    public MedianFinder() {
+        this.size = 0;
+        // Default is a maxHeap
+        this.maxHeap = new PriorityQueue<>();
+        this.minHeap = new PriorityQueue<>((a, b) -> {
+            return b.compareTo(a);
+        });
+    }
+    
+    // Time: O(1)
+    public void addNum(int num) {
+        if (size % 2 == 0) { // even -> odd
+            minHeap.offer(num);
+            maxHeap.offer(minHeap.poll());
+        } else { // odd -> even
+            maxHeap.offer(num);
+            minHeap.offer(maxHeap.poll());
+        }
+        size++;
+    }
+    
+    // Time: O(log n)
+    public double findMedian() {
+        if (size % 2 == 0) {
+            int l = maxHeap.peek(), r = minHeap.peek();
+            return (double)(l+r) / 2.0;
+        } else {
+            return (double) maxHeap.peek();
+        }
+    }
+    */
 }
 
 /**
