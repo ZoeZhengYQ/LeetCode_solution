2d52bb5953bb79b9ef39f673b4e9ae8388be3898
ZoeZhengYQ <yingqiao.zoe@gmail.com>
2020-01-13 13:23:39 -0500

    Brute Force DP

--- hard/472_concatenated_words.java
+++ hard/472_concatenated_words.java
@@ -1,8 +1,50 @@
 class Solution {
+	// 2020.1.13
+    // Brute force: DP
+    // Time: O(n*k^2), n: number of words, k: length of single word
+    // Space: O(n*k)
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Set<String> set = new HashSet<>();
+        List<String> res = new ArrayList<>();
+        for (String word : words) { set.add(word); }
+        for (String word : words) {
+            if (helper(set, word)) {
+                res.add(word);
+            }
+        }
+        return res;
+    }
+    
+    private boolean helper(Set<String> set, String word) {
+        int l = word.length();
+        if (l == 0) { return false; }
+        // cnt array remember that at position i, there's cnt[i] words that can be concatenated
+        int[] cnt = new int[l];
+        for (int i = 0; i < l; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (cnt[j] > 0) { 
+                    // If current position have a previous word, 
+                    // check if j+1 to i is a valid word
+                    if (set.contains(word.substring(j+1, i+1))) {
+                        cnt[i] = cnt[j]+1;
+                        // Do NOT forget the break, or it will TLE
+                        break;
+                    }
+                }
+            }
+            if (set.contains(word.substring(0, i+1))) {
+                cnt[i] = Math.max(cnt[i], 1);
+            }
+        }
+        // Concatenated need AT LEAST TWO!
+        return cnt[l-1] > 1;
+    }
+
     // DP: N - word break
     // Time: O(n^2 m)
     // Space: O(n)
     //  n - largest length of word, m - num of words
+    /*
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         int len = words.length;
         List<String> res = new ArrayList<>();
@@ -42,4 +84,5 @@ class Solution {
         }
         return check[l-1] > 1;
     }
+    */
 }
